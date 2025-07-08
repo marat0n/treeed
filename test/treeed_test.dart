e7566fae7d1222f205aea28b5115c1970e0c80ac
Marat Gevorkyan <mgevorkyan@skolopendra.com>
2025-07-08 16:17:14 +0400

    Added tests

--- test/treeed_test.dart
+++ test/treeed_test.dart
@@ -1,1 +1,56 @@
-// Nothing here for now...
+import 'package:test/test.dart';
+import 'package:treeed/treeed.dart';
+
+void main() {
+  treeedStateTests();
+}
+
+void treeedStateTests() {
+  test('TreeedState updates listeners on `set` invokations.', () {
+    final state = TreeedState(0);
+    var gotNewVal = false;
+
+    state.listen((_) => gotNewVal = true);
+    state.set(1);
+    expect(gotNewVal, equals(true));
+
+    state.dispose();
+  });
+
+  test('TreeedState updated value is stored correctly.', () {
+    final state = TreeedState(0);
+    expect(state.get, equals(0));
+
+    state.set(1);
+    expect(state.get, equals(1));
+
+    state.listen((_) => state.quietSet(50));
+    state.set(2);
+    expect(state.get, equals(50));
+
+    state.dispose();
+  });
+
+  test('TreeedState unlistens the observer-function.', () {
+    final state = TreeedState(0);
+    var updatingToggler = false;
+
+    observer(_) => updatingToggler = !updatingToggler;
+
+    state.listen(observer);
+    state.set(state.get);
+    expect(updatingToggler, equals(true));
+
+    state.unlisten(observer);
+    state.set(state.get);
+    expect(updatingToggler, equals(true));
+
+    state.dispose();
+  });
+
+  test('TreeedState provides correct new value for listenres (observers).', () {
+    final state = TreeedState(0);
+    state.listen((int newVal) => expect(newVal, equals(state.get)));
+    state.dispose();
+  });
+}
