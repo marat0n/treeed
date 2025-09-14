9a6a5807afa95765b2bc5f397b694b7559332d60
Marat Gevorkyan <mgevorkyan@skolopendra.com>
2025-09-14 21:37:23 +0400

    Add tests for conditional treeed state (`when` and `whenEquals` methods)

--- test/treeed_test.dart
+++ test/treeed_test.dart
@@ -54,4 +54,32 @@ void treeedStateTests() {
     state.set(1);
     state.dispose();
   });
+
+  test(
+    'TreeedState.when observation runs the provided action whenever the provided condition is returning true.',
+    () {
+      final state = ConditionalTreeedState(0);
+      state.when((x) => x % 2 == 0, (x) => state.set(x + 1));
+
+      state.set(1); // Does not call the action of `when` observer.
+      expect(state.get, equals(1));
+
+      state.set(2); // Does call the action and so incrementing the state value.
+      expect(state.get, equals(3));
+    },
+  );
+
+  test(
+    'TreeedState.whenEqual observation runs the provided action whenever the provided constant value is equal to a new updated value',
+    () {
+      final state = ConditionalTreeedState(0);
+      state.whenEquals(2, () => state.set(3));
+
+      state.set(1); // Does not call the action.
+      expect(state.get, equals(1));
+
+      state.set(2); // Does call the action and so set the state value as 3.
+      expect(state.get, equals(3));
+    },
+  );
 }
